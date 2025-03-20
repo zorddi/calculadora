ae06f00e7ed63d1d58c288b27c8a5c3ebac75985
zorddi <zorddi5@gmail.com>
2025-03-20 01:26:29 -0300

    Lógica JavaScript melhorada: html e css com pequenas mudanças.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # calculadora
+
 Estou criando minha versão da calculadora, um evento canônico para qualquer desenvolvedor web.
+
 ---
+
 [GitPages - Calculadora](https://zorddi.github.io/calculadora/)
--- index.js
+++ index.js
@@ -10,83 +10,141 @@ const Get = {
     }
 }
 
-function flexEventListener(element, event, callback){
-    if (!event){
-        console.error('Função "flexEventListener()" encerrada: o parâmetro "event" está ausente. Verifique e forneça o evento corretamente.');
+function ifTemplate(element, param){
+    if (!element){
+        console.error('Função "ifTemplate" encerrada: o valor "element" permanece "undefined". Por favor, insira um elemento válido.');
         return null;
     }
 
-    if (typeof event !== "string"){
-        console.error('Função "flexEventListener()" encerrada: o parâmetro "event" deve ser uma string. Verifique e forneça um valor válido para o evento.');
+    if (!param){
+        console.error('Objeto "disabledAttr" encerrado: o valor "param" se encontra "undefined". Por favor, insira "add" ou "rm" como valor.');
         return null;
     }
 
-    if (typeof callback !== "function") {
-        console.error("Função 'flexEventListener()' encerrada: o parâmetro 'callback' deve ser uma função. Verifique e forneça uma função válida.");
+    if (typeof param !== "string"){
+        console.error('Objeto "disabledAttr" encerrado: o valor "param" não é uma "string". Por favor, insira "add" ou "rm" como valor.');
         return null;
+    } else {
+        if (param !== "add"){
+            if (param !== "rm"){
+                console.error('Objeto "disabledAttr" finalizado: qualquer coisa diferente de "add" ou "rm" será considerado inválido.');
+                return null;
+            }
+        }
     }
 
-    const addListener = (e) => {
-        e.addEventListener(event, function(){
-            callback.call(this);
-        });
-    }
+    if (param === "add"){
+        if (element.getAttribute('disabled') === true){
+            return;
+        }
 
-    if (element instanceof NodeList || Array.isArray(element)){
-        element.forEach(addListener);
-    } else{
-        addListener(element);
+        element.setAttribute('disabled', 'true');
+    } else {
+        if (element.getAttribute('disabled') === null){
+            return;
+        }
+
+        element.removeAttribute('disabled');
     }
 }
 
-// Selecionando elementos e colocando em uma variável
+const disabledAttr = {
+    operator(param){
+        typeOperator.forEach((operator) => {
+            ifTemplate(operator, param);
+        });
+
+        return this;
+    },
+    point(param){
+        ifTemplate(point, param);
+
+        return this;
+    }
+};
+
+// Selecionando IDs
+const display = Get.id('display');
+const point = Get.id('point');
+const equalButton = Get.id('equal');
 const clear = Get.id('clear');
-const output = Get.id('output');
+
+// Selecionando Class
 const buttons = Get.queryAll('.button');
 const typeNumber = Get.queryAll('.number');
 const typeOperator = Get.queryAll('.operator');
-const point = Get.id('point');
-const equalButton = Get.id('equal');
 
-flexEventListener(buttons, 'click', function(){
-    output.value += this.textContent;
+// Se o atributo "readonly" for removido, ele será inserido logo em seguida
+let observer = new MutationObserver(() => {
+    if (!display.hasAttribute('readonly')){
+        display.setAttribute('readonly', 'true');
+    }
+});
+observer.observe(display, {
+    attributes: true,
+    subtree: true,
 });
 
-flexEventListener(typeNumber, 'click', function(){
-    typeOperator.forEach((operator) => {
-        operator.removeAttribute('disabled');
+function regex(){
+    if (/[0-9]+$/.test(display.value) === true){
+        operatorClicked = false;
+        disabledAttr.operator('rm');
+    } else if (/[0.-9]+$/.test(display.value) === true){
+        disabledAttr.point('add');
+    } else if (/[+-÷×]$/.test(display.value) === true){
+        operatorClicked = true;
+        disabledAttr.operator('add').point('rm');
+    }
+}
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Backspace'){
+
+        if (display.value === ''){
+            return;
+        } else{
+            display.value = display.value.slice(0, -1);
+        }
+        regex();
+    } else{
+        return;
+    }
+});
+
+// Escreve no display
+buttons.forEach((e) => {
+    e.addEventListener('click', function(){
+        display.value += this.textContent;
+        regex();
     });
 });
 
-flexEventListener(typeOperator, 'click', function(){
-    typeOperator.forEach((all) => {
-        all.setAttribute('disabled', 'true');
-    });
-    point.removeAttribute('disabled');
+// Reseta a calculadora
+clear.addEventListener('click', function(){
+    numberClicked = false;
+    operatorClicked = false;
+    display.value = '';
+    disabledAttr.operator("add").point("rm");
 });
 
-flexEventListener(point, 'click', function(){
-    point.setAttribute('disabled', 'true');
-});
-
-flexEventListener(clear, 'click', function(){
-    output.value = '';
-    point.removeAttribute('disabled');
-    typeOperator.forEach(all => {
-        all.setAttribute('disabled', 'true');
-    });
-});
-
-flexEventListener(equal, 'click', function(){
-    const value = output.value.replace('÷', '/').replace('×', '*');
+// Realiza o calculo
+equal.addEventListener('click', function(event){
+    // Cálculo e tratamento
+    const value = display.value.replace('÷', '/').replace('×', '*');
     try {
         const calc = math.evaluate(value);
         if (isNaN(calc)) {
-            output.value = 'erro';
+            display.value = 'erro';
         } else {
-            output.value = calc;
+            display.value = calc;
         }
     } catch (e) {
-        output.value = 'erro';
+        display.value = 'erro';
+    }
+
+    // Verifica se o resultado já possui vírgula:
+    // se sim, "point" não será interativo até que o próximo operador ser definido.
+    if (/\./.test(display.value) === true){
+        disabledAttr.point("add");
     }
 });
